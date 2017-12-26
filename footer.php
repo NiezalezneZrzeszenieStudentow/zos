3c317e0f28add3d1054f0ee914fd571b07b349cb
Karol <kazer94@gmail.com>
2017-12-26 18:14:53 +0100

    Struktura strony głównej

---
+++ footer.php
@@ -0,0 +1,60 @@
+﻿
+<footer>
+	<div class="container box-all">
+    	<div class="row">
+        	<div class="col-md-4">
+            <img src="img/d.jpg" alt="Logo ZOS">
+            	<h5>O nas</h5>
+            	<p>Eventually, by 2011 our small news startup have grown into a full-blown news agency, employing over 56 writers, journalists, editors and investigators, with our coverage spanning from such topics as the politics, sports, and entertainment all the way to health, science, and technologies.</p>
+            </div>
+            <div class="col-md-4">
+            	
+            	<h5>Kontakt</h5>
+                <div class="phone">
+                	<div class="contact-left"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                    <div class="contact-right">
+                    <p>Numer telefonu</p> 
+                    <p>tel. +48 61 665 28 51; +48 61 665 28 50</p>
+                    <p>fax. +48 61 665 22 00</p>
+                    </div>
+                </div>
+                <div class="place">
+                	<div class="contact-left"><i class="fa fa-globe" aria-hidden="true"></i></div>
+                    <div class="contact-right">
+                    <p>Adres</p> 
+                    <p>ul. Piotrowo 3</p>
+                    <p>60-965 Poznań</p>
+                    </div>
+                </div>
+                <div class="email">
+                	<div class="contact-left"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+                    <div class="contact-right">
+                    	<p>E-mail</p>
+                        <p>pawel.twardowski@put.poznan.pl</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+            
+            </div>
+        </div>
+    </div>
+</footer>
+<section class="footer-bottom">
+	<div class="container">
+    	<p><?php date('Y'); ?> © Zakład obróbki Skrawaniem | Wykonanie: <a href="">Karol Znojkiewicz</a></p>
+    </div>
+</section>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="bootstrap/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrap/assets/js/vendor/popper.min.js"></script>
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="bootstrap/assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,83 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ZOS - Strona główna</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.min.css">
+<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+<div class="header-links">
+	<div class="container">
+    	<div class="row">
+        	<div class="col-md-8">
+            
+            </div>
+            <div class="col-md-4">
+            	<ul class="language">
+                	<a href=""><li><img src="img/flags-mini/pl.png" width="25" height="15" alt="Polski"></li></a>
+                    <a href=""><li><img src="img/flags-mini/gb.png" width="25" height="15" alt="Angielski"></li></a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<header>
+	<div class="container">
+    	<img src="img/d.jpg" alt="Logo ZOS">
+    </div>
+</header>
+
+ <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="first-slide" src="img/mt-0969-content-bg01.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption d-none d-md-block text-left">
+              <h1>Example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="second-slide" src="img/mt-0969-content-bg01.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption d-none d-md-block">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="third-slide" src="img/mt-0969-content-bg01.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption d-none d-md-block text-right">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
