fd7cac60870320b7932b9bed791a31bb5de5e652
Karol <kazer94@gmail.com>
2017-12-26 19:46:44 +0100

    Zmodyfikowana strona główna

--- header.php
+++ header.php
@@ -19,18 +19,45 @@
             
             </div>
             <div class="col-md-4">
-            	<ul class="language">
+                	
+            	<ul class="help-top">
+               		<a href=""><li>Wydziały</li></a>
                 	<a href=""><li><img src="img/flags-mini/pl.png" width="25" height="15" alt="Polski"></li></a>
                     <a href=""><li><img src="img/flags-mini/gb.png" width="25" height="15" alt="Angielski"></li></a>
+                    
+                    <a href=""><li><i class="fa fa-sign-in" aria-hidden="true"></i>
+Zaloguj się</li></a>
                 </ul>
             </div>
         </div>
     </div>
 </div>
 <header>
-	<div class="container">
-    	<img src="img/d.jpg" alt="Logo ZOS">
-    </div>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <a class="navbar-brand" href="#"><img src="img/d.jpg" alt="Logo"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kadra</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Publikacje</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Dydaktyka</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Kontakt</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
 </header>
 
  <div id="myCarousel" class="carousel slide" data-ride="carousel">
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 ﻿<?php include('header.php'); ?>
 
+<section class="what-we-do">
+	<div class="container">
+        <h1>Co robimy?</h1>
+        <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</span>
+    
+    </div>
+</section>
 
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.2507820547335!2d16.94811051555974!3d52.40213097979207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47045b1252b17e69%3A0xfd1f08b05ec5737e!2sWydzia%C5%82+Budowy+Maszyn+i+Zarz%C4%85dzania+PP!5e0!3m2!1spl!2spl!4v1514309953965" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 <?php include('footer.php'); ?>
